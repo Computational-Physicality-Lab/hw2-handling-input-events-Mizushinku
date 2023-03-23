95b6aa40e58e45b2dd44e8ca11c0dec5cca94354
Mizushinku <frankie786214593@gmail.com>
2023-03-23 17:36:05 +0800

    first commit

--- input.js
+++ input.js
@@ -6,3 +6,8 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
+targets = document.getElementsByClassName("target");
+
+targets.forEach((target) => {
+  console.log(target);
+});
