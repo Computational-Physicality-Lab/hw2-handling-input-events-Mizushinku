c91f3f61f5124387332afa4697e26b3b198c0708
Mizushinku <frankie786214593@gmail.com>
2023-03-24 18:13:38 +0800

    pointer event

--- input.js
+++ input.js
@@ -6,8 +6,57 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
-targets = document.getElementsByClassName("target");
+const workspace = document.getElementById("workspace");
+const targets = document.querySelectorAll(".target");
+
+function initWorkspace() {
+  //   let showDot = false;
+  //   workspace.addEventListener("pointerdown", (e) => {
+  //     if (showDot) {
+  //       const dot = document.createElement("div");
+  //       dot.classList.add("dot");
+  //       dot.id = e.pointerId;
+  //       paintDot(e, dot);
+  //       document.body.append(dot);
+  //     }
+  //   });
+  //   workspace.addEventListener("pointermove", (e) => {
+  //     if (showDot) {
+  //       const dot = document.getElementById(e.pointerId);
+  //       if (dot == null) return;
+  //       paintDot(e, dot);
+  //     }
+  //   });
+  //   workspace.addEventListener("pointerup", (e) => {
+  //     if (showDot) {
+  //       const dot = document.getElementById(e.pointerId);
+  //       if (dot == null) return;
+  //       dot.remove();
+  //     }
+  //   });
+}
+function paintDot(e, dot) {
+  let scaler = 15;
+  dot.style.width = `${e.width * scaler}px`;
+  dot.style.height = `${e.height * scaler}px`;
+  dot.style.left = `${e.clientX}px`;
+  dot.style.top = `${e.clientY}px`;
+}
+function selection(e) {
+  console.log(e.target);
+  console.log(document.activeElement);
+  console.log(e.target === document.activeElement);
+}
+initWorkspace();
 
 targets.forEach((target) => {
-  console.log(target);
+  target.addEventListener("pointerdown", (e) => {
+    console.log(`Down, target = ${e.target.style.top}`);
+    e.stopPropagation();
+  });
+  target.addEventListener("pointerup", (e) => {
+    console.log(`Up, target = ${e.target.style.top}`);
+    selection(e);
+    e.stopPropagation();
+  });
 });
