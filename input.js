860ee265b4d922de084869c08436fe3c69108372
Mizushinku <frankie786214593@gmail.com>
2023-03-25 18:05:09 +0800

    long click

--- input.js
+++ input.js
@@ -6,57 +6,121 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    if (longPressTarget) {
+      longPressTarget.style.top = backup.top;
+      longPressTarget.style.left = backup.left;
+      longPressTarget = null;
+    }
+  }
+});
+
+/******* WorkSpace Code Zone *******/
 const workspace = document.getElementById("workspace");
+
+var isWsDown = false;
+
+workspace.addEventListener("pointerdown", () => {
+  console.log("WS Down");
+  isWsDown = true;
+});
+workspace.addEventListener("pointerup", () => {
+  console.log("WS Up");
+  longPressTarget = null;
+});
+workspace.addEventListener("click", (e) => {
+  if (isWsDown) {
+    console.log("WS Click");
+    if (focusedTarget) {
+      focusedTarget.style.backgroundColor = "red";
+      focusedTarget = null;
+    }
+    isWsDown = false;
+  }
+});
+workspace.addEventListener("pointermove", (e) => {
+  if (e.isPrimary && longPressTarget) {
+    longPressTarget.style.left = `${e.clientX}px`;
+    longPressTarget.style.top = `${e.clientY}px`;
+  }
+});
+
+/******* Targets Code Zone *******/
 const targets = document.querySelectorAll(".target");
 
-function initWorkspace() {
-  //   let showDot = false;
-  //   workspace.addEventListener("pointerdown", (e) => {
-  //     if (showDot) {
-  //       const dot = document.createElement("div");
-  //       dot.classList.add("dot");
-  //       dot.id = e.pointerId;
-  //       paintDot(e, dot);
-  //       document.body.append(dot);
-  //     }
-  //   });
-  //   workspace.addEventListener("pointermove", (e) => {
-  //     if (showDot) {
-  //       const dot = document.getElementById(e.pointerId);
-  //       if (dot == null) return;
-  //       paintDot(e, dot);
-  //     }
-  //   });
-  //   workspace.addEventListener("pointerup", (e) => {
-  //     if (showDot) {
-  //       const dot = document.getElementById(e.pointerId);
-  //       if (dot == null) return;
-  //       dot.remove();
-  //     }
-  //   });
-}
-function paintDot(e, dot) {
-  let scaler = 15;
-  dot.style.width = `${e.width * scaler}px`;
-  dot.style.height = `${e.height * scaler}px`;
-  dot.style.left = `${e.clientX}px`;
-  dot.style.top = `${e.clientY}px`;
-}
-function selection(e) {
-  console.log(e.target);
-  console.log(document.activeElement);
-  console.log(e.target === document.activeElement);
-}
-initWorkspace();
+var focusedTarget = null;
+var duringFirstTap = false;
+var longPressTimer = null;
+var longPressTarget = null;
+var backup = {
+  top: null,
+  left: null,
+  width: null,
+  height: null,
+};
 
 targets.forEach((target) => {
+  target.addEventListener("click", targetOnClick);
+  target.addEventListener("dblclick", (e) => {
+    console.log(`Double   Click, target = ${e.target.style.top}`);
+  });
+
   target.addEventListener("pointerdown", (e) => {
-    console.log(`Down, target = ${e.target.style.top}`);
     e.stopPropagation();
+    console.log(`Down, target = ${e.target.style.top}`);
+    longPressTimer = setTimeout(longPress.bind(e), 500);
   });
   target.addEventListener("pointerup", (e) => {
-    console.log(`Up, target = ${e.target.style.top}`);
-    selection(e);
     e.stopPropagation();
+    console.log(`Up, target = ${e.target.style.top}`);
+    if (longPressTarget !== e.target) {
+      longPressTarget = null;
+    }
+    clearTimeout(longPressTimer);
+
+    //TouchUp -> Click
+    if (e.pointerType == "touch") {
+      targetOnClick(e);
+    }
+  });
+
+  target.addEventListener("pointercancel", (e) => {
+    console.log(`Cancel, target = ${e.target.style.top}`);
+  });
+  target.addEventListener("pointerout", (e) => {
+    // console.log(`Out, target = ${e.target.style.top}`);
+    clearTimeout(longPressTimer);
+  });
+
+  target.addEventListener("touchstart", (e) => {
+    e.preventDefault();
   });
 });
+
+function longPress() {
+  let e = this; //long press event "e".
+  console.log(`LP ${e.target.style.top}`);
+  longPressTarget = e.target;
+  backup.top = e.target.style.top;
+  backup.left = e.target.style.left;
+}
+
+function setFocus(ele) {
+  if (focusedTarget) {
+    focusedTarget.style.backgroundColor = "red";
+  }
+  focusedTarget = ele;
+  focusedTarget.style.backgroundColor = "#00f";
+}
+
+function targetOnClick(e) {
+  e.stopPropagation();
+  if (longPressTarget) {
+    longPressTarget = null;
+    return;
+  }
+  console.log(`Click, target = ${e.target.style.top}`);
+  setFocus(e.target);
+}
